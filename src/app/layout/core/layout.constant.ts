a6e43eafbc168b133c404e509ba6384431f65ce7
ankitkumarsharma <aksharma.dixit@gmail.com>
2025-07-16 02:07:21 +0530

    update design components and utils

---
+++ src/app/layout/core/layout.constant.ts
@@ -0,0 +1,36 @@
+
+export const HEADER = {
+    logo: '< Angular Snippets />',
+    menuList: [
+        {
+            title:'Home',
+            url: null,
+            route:'/'
+        },
+        {
+            title:'About Us',
+            url: null,
+            route:'/'
+        },
+        {
+            title:'Features',
+            url: null,
+            route:'/'
+        },
+        {
+            title:'Tags',
+            url: null,
+            route:'/'
+        },
+        {
+            title:'Contribute',
+            url:'https://github.com/ankitkumarsharma/angular-snippets',
+            route: null
+        },
+        {
+            title:'Contact Us',
+            url: null,
+            route:'/'
+        }
+    ]
+}
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { SharedService } from '../../shared/services/shared.service';
+import { HEADER } from '../core/layout.constant';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './header.scss'
 })
 export class Header {
-  logo:string = '< Angular Snippets />'
+  logo:string = HEADER.logo;
+  menu = HEADER.menuList;
+  
+  showComponentContainer = inject(SharedService);
+  showTagContainer = inject(SharedService);
+  router = inject(Router);
+
+  onRouteFn(item:any) {
+    this.showComponentContainer.showComponentContainer.set(false);
+    this.showTagContainer.showTagContainer.set(true);
+    this.router.navigate([item.route]);
+  }
 }
